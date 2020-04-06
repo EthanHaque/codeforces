a1e62fee7bfa3f9bad0f6384680b44992444df5d
EH <43977335+EthanHaque@users.noreply.github.com>
2020-04-06 15:24:30 -0500

    moving

--- FafaAndHisCompany/FafaAndHisCompany935A.py
+++ FafaAndHisCompany/FafaAndHisCompany935A.py
@@ -1,11 +1,9 @@
 n = int(input())
-result = []
+result = 0
 i = 1
-while( i*i <= n):
-    if n % i == 0:
-        result.append(i)
-        if n//i != i: 
-            result.append(n//i)
+while( i <= n/2):
+    if( (n-i) % i == 0):
+        result += 1
     i += 1
 
 print(result)
